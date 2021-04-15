5e953282101695159287e30bf7ffa9253f5de204
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-04-15 15:32:20 -0500

    More tools installed to the Design System like minify CSS, concat & watch.

--- Platzi_DS/gulpfile.js
+++ Platzi_DS/gulpfile.js
@@ -1,14 +1,26 @@
 var gulp = require('gulp')
-var sass = require('gulp-scss')
+var sass = require('gulp-sass')
+var minifyCSS = require ('gulp-minify-css')
+var concat = require('gulp-concat')
 
-function is not longer working properly. Have to use arrow functions instead.
 
-gulp.task('Hello There', function () {
-  console.log('Hello There!')
+/* function is not longer working properly. Have to use arrow functions instead. */
+
+gulp.task('hello', done => {
+  console.log('Hello There!!!')
+  done()
 })
 
 gulp.task('sass', function () {
   return gulp.src('scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('public/stylesheets'))
+})
+
+gulp.task('style_min', function () {
+  return gulp.src('src/**/*.scss')
+  .pipe(sass())
+  .pipe(minifyCSS())
+  .pipe(concat('style_main.min.css'))
+  .pipe(gulp.dest('public/stylesheets'))
 })
